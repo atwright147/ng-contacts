7be1b2920271b75aa9d01253ca60271d77c635c4
Andy Wright <atwright147@gmail.com>
2019-01-11 10:18:50 +0000

    create a service to get contacts

--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
+  base_url = 'http://localhost:8882';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getContactsList() {
+    this.http.get<Array<any>>(`${this.base_url}/contacts`);
+  }
+
+  getContactById(contactId: number) {
+    this.http.get<Array<any>>(`${this.base_url}/contact/${contactId}`);
+  }
 }
