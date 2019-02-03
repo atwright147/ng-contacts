e3da35dddace7706f94177d91a74a41aa66262ba
Andy Wright <atwright147@gmail.com>
2019-02-03 21:35:01 +0000

    add some tests for correlation id interceptor

--- src/app/http-interceptors/correlation-id.interceptor.spec.ts
+++ src/app/http-interceptors/correlation-id.interceptor.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed } from '@angular/core/testing';
-
-import { AppComponent } from '../app.component';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from '../app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from '../app.component';
+import { AppRoutingModule } from '../app-routing.module';
 import { ContactsListComponent } from '../components/contacts-list/contacts-list.component';
 import { ContactsDetailComponent } from '../components/contacts-detail/contacts-detail.component';
 import { ContactsFormComponent } from '../components/contacts-form/contacts-form.component';
@@ -13,9 +15,13 @@ import { NotificationComponent } from '../components/notification/notification.c
 import { HeaderComponent } from '../components/header/header.component';
 import { FooterComponent } from '../components/footer/footer.component';
 
+import { ContactService } from '../services/contact.service';
 import { CorrelationIdInterceptor } from './correlation-id.interceptor';
 
 describe('CorrelationIdInterceptor', () => {
+  let service: ContactService;
+  let httpMock: HttpTestingController;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [
@@ -32,17 +38,35 @@ describe('CorrelationIdInterceptor', () => {
         BrowserModule,
         AppRoutingModule,
         HttpClientModule,
+        HttpClientTestingModule,
         FormsModule,
         ReactiveFormsModule,
       ],
       providers: [
-        CorrelationIdInterceptor,
+        ContactService,
+        {
+          provide: HTTP_INTERCEPTORS,
+          useClass: CorrelationIdInterceptor,
+          multi: true,
+        },
       ],
     });
+
+    service = TestBed.get(ContactService);
+    httpMock = TestBed.get(HttpTestingController);
   });
 
   it('should be created', () => {
-    const service: CorrelationIdInterceptor = TestBed.get(CorrelationIdInterceptor);
     expect(service).toBeTruthy();
+  });
+
+  it('should add the X-Correlation-Id header', () => {
+    service.getContactsList().subscribe(response => {
+      expect(response).toBeTruthy();
+    });
+
+    const httpRequest = httpMock.expectOne(`${service.base_url}/contacts`);
+
+    expect(httpRequest.request.headers.has('X-Correlation-Id')).toEqual(true);
   });
 });
