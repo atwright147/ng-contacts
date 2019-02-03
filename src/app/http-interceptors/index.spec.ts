92e972b8e94341043fed0b8e1982794a1b5bfb9e
Andy Wright <atwright147@gmail.com>
2019-02-03 21:32:33 +0000

    remove fdescribe

--- src/app/http-interceptors/index.spec.ts
+++ src/app/http-interceptors/index.spec.ts
@@ -3,7 +3,7 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { httpInterceptorProviders } from './index';
 import { CorrelationIdInterceptor } from './correlation-id.interceptor';
 
-fdescribe('AppComponent', () => {
+describe('AppComponent', () => {
   it(`should have as title 'ng-contacts'`, () => {
     const expected: Array<any> = [
       { provide: HTTP_INTERCEPTORS, useClass: CorrelationIdInterceptor, multi: true },
