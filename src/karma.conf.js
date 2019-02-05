f29674ff63435d17f7b53bfb05f173bdeb63279f
Andy Wright <atwright147@gmail.com>
2019-02-05 22:34:37 +0000

    use mocha spec reporter because karma/jasmine reporter is shit

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -7,7 +7,7 @@ module.exports = function (config) {
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-spec-reporter'),
+      require('karma-mocha-reporter'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
@@ -21,12 +21,11 @@ module.exports = function (config) {
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'spec'],
+    reporters: ['progress', 'mocha'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    // browsers: ['Chrome'],
     browsers: ['ChromeHeadless'],
     flags: [
       '--disable-web-security',
