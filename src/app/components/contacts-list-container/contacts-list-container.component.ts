17757373e0403c7b68c4c7608f9edb496d00baf6
Andy Wright <atwright147@gmail.com>
2019-02-14 19:40:42 +0000

    fix some template issues

--- src/app/components/contacts-list-container/contacts-list-container.component.ts
+++ src/app/components/contacts-list-container/contacts-list-container.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import { ContactService } from '../../services/contact.service';
 
@@ -7,14 +7,19 @@ import { ContactService } from '../../services/contact.service';
   templateUrl: './contacts-list-container.component.html',
   styleUrls: ['./contacts-list-container.component.scss']
 })
-export class ContactsListContainerComponent implements OnInit {
+export class ContactsListContainerComponent implements OnInit, OnDestroy {
+  getContactsListSub;
   contacts: Array<any>;
 
   constructor(private Contact: ContactService) { }
 
   ngOnInit() {
-    this.Contact
+    this.getContactsListSub = this.Contact
       .getContactsList()
       .subscribe(data => this.contacts = data);
+  }
+
+  ngOnDestroy() {
+    this.getContactsListSub.unsubscribe();
   }
 }
