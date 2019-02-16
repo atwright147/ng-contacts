a4f370aecb8d94e383b0b6398603772060e9c370
Andy Wright <atwright147@gmail.com>
2019-02-16 00:23:51 +0000

    add tests for table data and row linkage

--- src/app/components/contacts-list/contacts-list.component.spec.ts
+++ src/app/components/contacts-list/contacts-list.component.spec.ts
@@ -1,12 +1,24 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
+
 import { Shallow } from 'shallow-render';
 
 import { ContactsListComponent } from './contacts-list.component';
 
+@Component({
+  template: ''
+})
+class DummyComponent {}
+
 @NgModule({
   declarations: [
     ContactsListComponent,
-  ]
+  ],
+  imports: [
+    RouterTestingModule.withRoutes([
+      { path: '/contacts/42', component: DummyComponent }
+    ])
+  ],
 })
 class TestModule {}
 
@@ -18,17 +30,30 @@ describe('ContactsListComponent', () => {
   });
 
   it('should render a table with correct head section', async () => {
-    const { find } = await shallow.render('<app-contacts-list [contacts]="contacts"></app-contacts-list>');
+    const contacts = [
+      { id: 42, firstName: 'Jimmy', surname: 'Tester', email: 'Jimmy.Tester@example.com', groupId: 3 }
+    ];
+    const { find } = await shallow.render({ bind: { contacts } });
     const columns = find('table').nativeElement.querySelectorAll('thead th');
 
     expect(columns.length).toEqual(4);
   });
 
+  it('should link table row element to correct url', async () => {
+    const contacts = [
+      { id: 42, firstName: 'Jimmy', surname: 'Tester', email: 'Jimmy.Tester@example.com', groupId: 3 }
+    ];
+    const { find } = await shallow.render({ bind: { contacts } });
+    const routerLink = find('table').nativeElement.querySelectorAll('tbody tr')[0].attributes['ng-reflect-router-link'].value;
+
+    expect(routerLink).toEqual('contact,42');
+  });
+
   it('should render a table with correct body', async () => {
     const contacts = [
-      { id: 2, firstName: 'Jimmy', surname: 'Tester', email: 'Jimmy.Tester@example.com', groupId: 3 }
+      { id: 42, firstName: 'Jimmy', surname: 'Tester', email: 'Jimmy.Tester@example.com', groupId: 3 }
     ];
-    const { find } = await shallow.render('<app-contacts-list [contacts]="contacts"></app-contacts-list>');
+    const { find } = await shallow.render({ bind: { contacts } });
     const columns = find('table').nativeElement.querySelectorAll('tbody td');
 
     expect(columns.length).toEqual(4);
